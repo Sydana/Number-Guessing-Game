677691a83d40bbef5a8eeab6b2634493143ef7e7
Sydana Schalk <sydana.schalk.2019@student.lhschools.org>
2018-09-21 14:12:26 -0400

    FINISHED LIMIT

--- src/Number_guessing_game.java
+++ src/Number_guessing_game.java
@@ -11,7 +11,6 @@ public class Number_guessing_game {
 
 
         keyboard = new Scanner(System.in);
-        System.out.println(pickedNumber);
 
         System.out.println("Welcome to the number guessing game!");
         System.out.println("This is how things are going to work:");
@@ -20,116 +19,247 @@ public class Number_guessing_game {
             guessedNumber = keyboard.nextInt();
             //first try
             if(guessedNumber==pickedNumber){
-                System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-            }
+                System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
             else if(guessedNumber>pickedNumber){
-                System.out.println("Nope. You aimed too high. Guess again. You have 3 tries left");
+                System.out.println("Nope. Too high. Guess again. You have 3 tries left");
                 guessedNumber = keyboard.nextInt();
+
                 //second try
                 if(guessedNumber==pickedNumber){
-                    System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                }
+                    System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                 else if(guessedNumber>pickedNumber){
-                    System.out.println("Nope. You aimed too high. Guess again. You have 2 tries left.");
+                    System.out.println("Nope. Too high. Guess again. You have 2 tries left");
                     guessedNumber = keyboard.nextInt();
+
                     //third try
                     if(guessedNumber==pickedNumber){
-                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                    }
+                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                     else if(guessedNumber>pickedNumber){
-                        System.out.println("Nope. You aimed too high. Guess again. This is your last try.");
+                        System.out.println("Nope. Too high. Guess again. You have 1 try left");
                         guessedNumber = keyboard.nextInt();
-                        //fourth try
+
+                        //final try
                         if(guessedNumber==pickedNumber){
-                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                        }
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                         else if(guessedNumber>pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
                             guessedNumber = keyboard.nextInt();
+
+
                         }
                         else if(guessedNumber<pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
                             guessedNumber = keyboard.nextInt();
                         }
+
                     }
                     else if(guessedNumber<pickedNumber){
-                        System.out.println("Nope. You aimed too low. Guess again. This is your last try.");
+                        System.out.println("Nope. Too low. Guess again. You have 1 try left.");
                         guessedNumber = keyboard.nextInt();
-                        //fourth try
+
+                        //final try
                         if(guessedNumber==pickedNumber){
-                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                        }
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                         else if(guessedNumber>pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too high. It was"+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
 
                         }
                         else if(guessedNumber<pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too low. It was"+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
                         }
-                    }
-                    }
 
+                    }
                 }
                 else if(guessedNumber<pickedNumber){
-                    System.out.println("Nope.dfg You aimed too low. Guess again. You have 3 tries left.");
+                    System.out.println("Nope. Too low. Guess again. You have 2 tries left.");
                     guessedNumber = keyboard.nextInt();
+
+                    //third try
+                    if(guessedNumber==pickedNumber){
+                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                    else if(guessedNumber>pickedNumber){
+                        System.out.println("Nope. Too high. Guess again. You have 1 try left");
+                        guessedNumber = keyboard.nextInt();
+
+                        //final try
+                        if(guessedNumber==pickedNumber){
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                        else if(guessedNumber>pickedNumber){
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
+
+                        }
+                        else if(guessedNumber<pickedNumber){
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+                        }
+
+                    }
+                    else if(guessedNumber<pickedNumber){
+                        System.out.println("Nope. Too low. Guess again. You have 1 try left.");
+                        guessedNumber = keyboard.nextInt();
+
+                        //final try
+                        if(guessedNumber==pickedNumber){
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                        else if(guessedNumber>pickedNumber){
+                            System.out.println("Nope. Too high. It was"+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
+
+                        }
+                        else if(guessedNumber<pickedNumber){
+                            System.out.println("Nope. Too low. It was"+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+                        }
+
+                    }
+                }
+
+            }
+            else if(guessedNumber<pickedNumber){
+                System.out.println("Nope. Too low. Guess again. You have 3 tries left.");
+                guessedNumber = keyboard.nextInt();
 
                 //second try
                 if(guessedNumber==pickedNumber){
-                    System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                }
+                    System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                 else if(guessedNumber>pickedNumber){
-                    System.out.println("Nope. You aimed too high. Guess again. You have 2 tries left.");
+                    System.out.println("Nope. Too high. Guess again. You have 2 tries left");
                     guessedNumber = keyboard.nextInt();
+
                     //third try
                     if(guessedNumber==pickedNumber){
-                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                    }
+                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                     else if(guessedNumber>pickedNumber){
-                        System.out.println("Nope. You aimed too high. Guess again. This is your last try.");
+                        System.out.println("Nope. Too high. Guess again. You have 1 try left");
                         guessedNumber = keyboard.nextInt();
-                        //fourth try
+
+                        //final try
                         if(guessedNumber==pickedNumber){
-                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                        }
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                         else if(guessedNumber>pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
                             guessedNumber = keyboard.nextInt();
+
+
                         }
                         else if(guessedNumber<pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
                             guessedNumber = keyboard.nextInt();
                         }
+
                     }
                     else if(guessedNumber<pickedNumber){
-                        System.out.println("Nope. You aimed too low. Guess again. This is your last try.");
+                        System.out.println("Nope. Too low. Guess again. You have 1 try left.");
                         guessedNumber = keyboard.nextInt();
-                        //fourth try
+
+                        //final try
                         if(guessedNumber==pickedNumber){
-                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");
-                        }
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
                         else if(guessedNumber>pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
 
                         }
                         else if(guessedNumber<pickedNumber){
-                            System.out.println("Nope. You lost. Too bad. It was "+pickedNumber+".");
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
                         }
+
+                    }
+                }
+                else if(guessedNumber<pickedNumber){
+                    System.out.println("Nope. Too low. Guess again. You have 2 tries left.");
+                    guessedNumber = keyboard.nextInt();
+
+                    //third try
+                    if(guessedNumber==pickedNumber){
+                        System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                    else if(guessedNumber>pickedNumber){
+                        System.out.println("Nope. Too high. Guess again. You have 1 try left");
+                        guessedNumber = keyboard.nextInt();
+
+                        //final try
+                        if(guessedNumber==pickedNumber){
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                        else if(guessedNumber>pickedNumber){
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
+
+                        }
+                        else if(guessedNumber<pickedNumber){
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+                        }
+
+                    }
+                    else if(guessedNumber<pickedNumber){
+                        System.out.println("Nope. Too low. Guess again. You have 1 try left.");
+                        guessedNumber = keyboard.nextInt();
+
+                        //final try
+                        if(guessedNumber==pickedNumber){
+                            System.out.println("Wow, you actually did it. Good job! Congrats, you won the game.");}
+
+                        else if(guessedNumber>pickedNumber){
+                            System.out.println("Nope. Too high. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+
+
+                        }
+                        else if(guessedNumber<pickedNumber){
+                            System.out.println("Nope. Too low. It was "+pickedNumber+". You lose.");
+                            guessedNumber = keyboard.nextInt();
+                        }
+
                     }
                 }
                 }
-                }
-
-            }
-
-
-            }
 
 
 
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
